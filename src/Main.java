5a1f78b254d6e57a307844a009694b4fa7833868
kanoyang <a0928345634@gmail.com>
2024-02-16 22:33:23 +0800

    反轉陣列

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,47 @@
 
     import javax.swing.*;
     import java.awt.*;
+    import java.awt.event.KeyEvent;
+    import java.awt.event.KeyListener;
     import java.text.DecimalFormat;
     import java.util.*;
-    public class Main {
+
+
+    public class Main extends JPanel implements KeyListener{
+        public Main(){
+            addKeyListener(this);
+        }
+
+        @Override
+        public void paintComponent(Graphics g){
+            requestFocusInWindow();
+
+        }
+
         public static void main(String[] args) {
 
-        Edible chicken = new Chicken();
-        chicken.howToEat();
-            System.out.println(chicken.howToEat());
+        JFrame windows = new JFrame();
+        windows.setSize(500,500);
+        windows.setContentPane(new Main());
+        windows.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        windows.setVisible(true);
 
 
+        }
 
+        @Override
+        public void keyTyped(KeyEvent e) {
+
+        }
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            System.out.println(e.getKeyCode());
+
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
 
         }
     }
