46a0e245465978c6365bcca24d7835c38756dd13
kanoyang <a0928345634@gmail.com>
2024-07-28 22:07:32 +0800

    上課中 add sheep class

--- src/Main.java
+++ src/Main.java
@@ -9,37 +9,67 @@
     import java.util.*;
 
 
-public class Main
-{
+public class Main extends JPanel implements KeyListener{
+
+    Sheep s;
+
+    public Main(){
+
+        s =new Sheep(40,40);
+        addKeyListener(this);
+    }
+    @Override
+    public void paintComponent(Graphics g)
+    {
+        g.setColor(Color.BLACK);
+        g.fillRect(0,0,getWidth(),getHeight());
+        s.drawSheep(g);
+        requestFocusInWindow();
+
+    }
+
     public static void main(String[] args) {
+        JFrame windows = new JFrame();
+        windows.setContentPane(new Main());
+        windows.setSize(500, 500);
+        windows.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        windows.setVisible(true);//?
+    }
 
+    @Override
+    public void keyTyped(KeyEvent e) {
 
-//        JFrame window = new JFrame();
-//        window.setSize(500,500);
-//        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//
-//        window.setContentPane(new MyPane());
-//        window.setVisible(true);
+    }
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int k = e.getKeyCode();
+        switch (k){
+            case KeyEvent.VK_UP :
+                s.walk(0,-5);
+                System.out.println(s.toString());
+                break;
+            case KeyEvent.VK_DOWN:
+                s.walk(0,5);
+                System.out.println(s.toString());
+                break;
+            case KeyEvent.VK_RIGHT:
+                s.walk(5,0);
+                System.out.println(s.toString());
+                break;
+            case KeyEvent.VK_LEFT:
+                s.walk(-5,0);
+                System.out.println(s.toString());
+                break;
+            default:
+                JOptionPane.showMessageDialog(this,"Please use an error key");
+        }
+        repaint();
+        //System.out.println(s);
+        //System.out.println(k);
+    }
 
-       People s1  = new Student("Willson",25,"hawaii",98);
-       People t1 = new Teacher("Grace",33,"Hawiii","CS");
-       People t2 = new Teacher("Gary",22,"Taiwan","English");
-
-       ArrayList<People> people = new ArrayList<>();
-       people.add(s1);
-       people.add(t1);
-       people.add(t2);
-
-
-
-
-
-
-
-
-
-
-
+    @Override
+    public void keyReleased(KeyEvent e) {
 
     }
 }
